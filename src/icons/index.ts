961fb6761d0af3fa4361cf6265b00e3674d47156
sosnovichroman <sosnovichroman@gmail.com>
2025-07-09 16:10:45 +0300

    усложнение структуры каталогов

---
+++ src/icons/index.ts
@@ -0,0 +1,8 @@
+const sayHello = (name: string) => {
+  return `Hey ${name}, say hello to TypeScript.`;
+};
+const sayBye = (name: string) => {
+  return `Hey ${name}, say bye to TypeScript.`;
+};
+
+export { sayBye, sayHello };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-const sayHello = (name: string) => {
-  return `Hey ${name}, say hello to TypeScript.`;
-};
-const sayBye = (name: string) => {
-  return `Hey ${name}, say bye to TypeScript.`;
-};
-
-export { sayBye, sayHello };
+export * from "./icons";
