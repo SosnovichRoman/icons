be2da137355eceeae7ca2e33739ec36eca050ed1
sosnovichroman <sosnovichroman@gmail.com>
2025-07-09 12:32:00 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ const sayHello = (name: string) => {
   return `Hey ${name}, say hello to TypeScript.`;
 };
 
-export default sayHello;
+const sayBye = (name: string) => {
+  return `Hey ${name}, say bye to TypeScript.`;
+};
+
+export { sayHello, sayBye };
