11b93ceb6dc64a08c802a3211f75a9d2a3a74c08
sosnovichroman <sosnovichroman@gmail.com>
2025-07-10 14:03:19 +0300

    создание svg компонентов

---
+++ native/index.js
@@ -0,0 +1,3 @@
+export { default as IconTrash } from './icons/iconTrash/iconTrash.svg.js';
+export { default as IconTrash2 } from './icons/iconTrash2/iconTrash2.svg.js';
+//# sourceMappingURL=index.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,25 @@
 import typescript from "@rollup/plugin-typescript";
 import svgr from "@svgr/rollup";
+// import * as react from "react";
+// import * as RNSVG from "react-native-svg";
 
 export default {
-  input: "build/native/index.ts",
+  input: "src/index.ts",
   output: {
-    file: "native/index.js",
+    // file: "native/index.js",
+    dir: "native",
     format: "esm",
     sourcemap: true,
+    preserveModules: true,
+    // external: ["react-native-svg", "react"],
   },
   plugins: [
     typescript(),
-    // svgr({
-    //   // native: true,
-    //   // typescript: true,
-    //   // exportType: "named",
-    //   // jsxRuntime: "automatic",
-    // }),
+    svgr({
+      native: true,
+      typescript: true,
+      // exportType: "named",
+      // jsxRuntime: "automatic",
+    }),
   ],
 };
