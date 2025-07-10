ec24e57e31adebc86bc5a67b01462a0b1b548c32
sosnovichroman <sosnovichroman@gmail.com>
2025-07-10 12:21:20 +0300

    Нерабочая команда для отдельной генерации

--- native/icons/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const sayHello: (name: string) => string;
-declare const sayBye: (name: string) => string;
-import IconTrash from "./iconTrash/iconTrash.svg";
-export { sayBye, sayHello, IconTrash };
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,20 @@
 import typescript from "rollup-plugin-typescript2";
 import svgr from "@svgr/rollup";
-import url from "@rollup/plugin-url";
 
 export default {
-  input: "src/index.ts",
+  input: "build/native/index.ts",
   output: {
     file: "native/index.js",
     format: "esm",
+    sourcemap: true,
   },
-  plugins: [typescript(), svgr({ typescript: true })],
+  plugins: [
+    typescript(),
+    // svgr({
+    //   // native: true,
+    //   // typescript: true,
+    //   // exportType: "named",
+    //   // jsxRuntime: "automatic",
+    // }),
+  ],
 };
