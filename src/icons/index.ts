792f6621e4799ab168cd198b14196dcd2b8d419a
sosnovichroman <sosnovichroman@gmail.com>
2025-07-10 12:21:20 +0300

    Нерабочая команда для отдельной генерации

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,10 +1,4 @@
-const sayHello = (name: string) => {
-  return `Hey ${name}, say hello to TypeScript.`;
-};
-const sayBye = (name: string) => {
-  return `Hey ${name}, say bye to TypeScript.`;
-};
-
 import IconTrash from "./iconTrash/iconTrash.svg";
+import IconTrash2 from "./iconTrash/iconTrash2.svg";
 
-export { sayBye, sayHello, IconTrash };
+export { IconTrash, IconTrash2 };
--- svgr.d.ts
+++ svgr.d.ts
@@ -1,12 +1,19 @@
 // Необходимо для того, чтобы typescript мог импортировать svg
 
 declare module "*.svg" {
-  import { FC, SVGProps } from "react";
-  const content: FC<SVGProps<SVGElement>>;
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
   export default content;
 }
 
-declare module "*.svg?url" {
-  const content: any;
-  export default content;
-}
+// declare module "*.svg" {
+//   import * as React from "react";
+//   const content: React.JSX.Element;
+//   export default content;
+// }
+
+// declare module "*.svg?url" {
+//   const content: any;
+//   export default content;
+// }
